0ae48a23e976881da8be12157f451980f9ab117a
Tran Vinh Kiet <tran.kiet.7946@gmail.com>
2025-03-25 21:47:28 +0700

    formyteacher

---
+++ Tuan1/Basic.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ int main() {
+    cout << "Hello, World!" << endl;
+    cout << "nhap 2 con so bat ki"<<endl;
+    int a,b;
+    cin >> a >> b;
+    cout << "tong 2 so la: " << a+b << endl;
+    return 0;
+ }
